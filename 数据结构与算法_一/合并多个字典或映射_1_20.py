8bccb16bb60780bceb986decfc8612f2ee403c19
wangmengmeng <wmm_0165@163.com>
2019-08-27 00:10:16 +0800

    update

--- 数据结构与算法_一/合并多个字典或映射_1_20.py
+++ 数据结构与算法_一/合并多个字典或映射_1_20.py
@@ -1,10 +1,12 @@
-# 如果出现重复键，那么第一次出现的键值会被返回
+# 逻辑上合并为单一的映射，如果出现重复键，那么第一次出现的键值会被返回
 from collections import ChainMap
 a = {'x':1, 'z':3}
 b = {'y':2, 'z':4}
 c = ChainMap(a, b)
+print(c)
 print(c['x'])
 print(c['z'])
 print(len(c))
-
+print(list(c.keys()))
+print(list(c.values()))
 
--- 数据结构与算法_一/解压可迭代对象赋值给多个变量.py
+++ 数据结构与算法_一/解压可迭代对象赋值给多个变量.py
@@ -1,3 +1,4 @@
 record = ('Dave','wangmm@ipharmcare.net','1234','5678')
 name, mail, *numbers = record
+print(numbers)  # 返回的是个列表
 print(numbers[0:2])
