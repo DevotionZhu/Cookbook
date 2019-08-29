30052233874a50eb85629500707d70c543bba167
wangmengmeng <wmm_0165@163.com>
2019-08-29 19:56:41 +0800

    删除不相关文件

--- test/run_all.py
+++
@@ -1,5 +0,0 @@
-import unittest
-from config.config import *
-
-logging.info("================================== 测试开始 ==================================")
-suite = unittest.defaultTestLoader.discover("./")
